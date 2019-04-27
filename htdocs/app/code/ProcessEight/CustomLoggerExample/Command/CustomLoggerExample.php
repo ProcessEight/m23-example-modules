dec1e5001451729a9148a17c8eac75bd07ba48fd
ProcessEight <s.frost@frostnet.co.uk>
2019-04-27 12:43:17 +0100

    ProcessEight_CustomLoggerExample: Added virtualType example; Updated command output; Updated README.md

--- htdocs/app/code/ProcessEight/CustomLoggerExample/Command/CustomLoggerExample.php
+++ htdocs/app/code/ProcessEight/CustomLoggerExample/Command/CustomLoggerExample.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpUndefinedClassInspection */
 /**
  * Process Eight
  *
@@ -30,16 +30,32 @@ class CustomLoggerExample extends Command
     private $logger;
 
     /**
+     * @var \ProcessEight\CustomLoggerExample\Logger\Logger
+     */
+    private $customLogger;
+
+    /**
+     * @var \Magento\Framework\Logger\Monolog
+     */
+    private $virtualTypeLogger;
+
+    /**
      * CustomLoggerExample constructor.
      *
-     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Psr\Log\LoggerInterface                        $logger
+     * @param \ProcessEight\CustomLoggerExample\Logger\Logger $customLogger
+     * @param \Magento\Framework\Logger\Monolog               $virtualTypeLogger
      */
     public function __construct(
-        \Psr\Log\LoggerInterface $logger
+        \Psr\Log\LoggerInterface $logger,
+        \ProcessEight\CustomLoggerExample\Logger\Logger $customLogger,
+        \Magento\Framework\Logger\Monolog $virtualTypeLogger
     ) {
         parent::__construct();
 
-        $this->logger = $logger;
+        $this->logger            = $logger;
+        $this->customLogger      = $customLogger;
+        $this->virtualTypeLogger = $virtualTypeLogger;
     }
 
     protected function configure()
@@ -51,8 +67,36 @@ class CustomLoggerExample extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("Hello World");
+        $output->writeln("Writing message with log level 'DEBUG' to var/log/debug.log using " . get_class($this->logger));
+        $this->logger->debug("This is a message written by " . get_class($this->logger) . " at " . date('Ymd H:i:s'));
+        $this->logger->info("This is a message written by " . get_class($this->logger) . " at " . date('Ymd H:i:s'));
+        $this->logger->notice("This is a message written by " . get_class($this->logger) . " at " . date('Ymd H:i:s'));
+        $this->logger->warning("This is a message written by " . get_class($this->logger) . " at " . date('Ymd H:i:s'));
+        $this->logger->error("This is a message written by " . get_class($this->logger) . " at " . date('Ymd H:i:s'));
+        $this->logger->critical("This is a message written by " . get_class($this->logger) . " at " . date('Ymd H:i:s'));
+        $this->logger->alert("This is a message written by " . get_class($this->logger) . " at " . date('Ymd H:i:s'));
+        $this->logger->emergency("This is a message written by " . get_class($this->logger) . " at " . date('Ymd H:i:s'));
 
-        $this->logger->debug("ABC123 This is a message written by ");
+        $output->writeln("Writing message with log level 'DEBUG' to var/log/custom_logger_example.log using " . get_class($this->customLogger));
+        $this->customLogger->debug("This is a message written by " . get_class($this->customLogger) . " at " . date('Ymd H:i:s'));
+        $this->customLogger->info("This is a message written by " . get_class($this->customLogger) . " at " . date('Ymd H:i:s'));
+        $this->customLogger->notice("This is a message written by " . get_class($this->customLogger) . " at " . date('Ymd H:i:s'));
+        $this->customLogger->warning("This is a message written by " . get_class($this->customLogger) . " at " . date('Ymd H:i:s'));
+        $this->customLogger->error("This is a message written by " . get_class($this->customLogger) . " at " . date('Ymd H:i:s'));
+        $this->customLogger->critical("This is a message written by " . get_class($this->customLogger) . " at " . date('Ymd H:i:s'));
+        $this->customLogger->alert("This is a message written by " . get_class($this->customLogger) . " at " . date('Ymd H:i:s'));
+        $this->customLogger->emergency("This is a message written by " . get_class($this->customLogger) . " at " . date('Ymd H:i:s'));
+
+        $output->writeln("Writing message with log level 'DEBUG' to var/log/custom_logger_example_virtual_type.log using " . get_class($this->virtualTypeLogger));
+        $this->virtualTypeLogger->debug("This is a message written by " . get_class($this->virtualTypeLogger) . " at " . date('Ymd H:i:s'));
+        $this->virtualTypeLogger->info("This is a message written by " . get_class($this->virtualTypeLogger) . " at " . date('Ymd H:i:s'));
+        $this->virtualTypeLogger->notice("This is a message written by " . get_class($this->virtualTypeLogger) . " at " . date('Ymd H:i:s'));
+        $this->virtualTypeLogger->warning("This is a message written by " . get_class($this->virtualTypeLogger) . " at " . date('Ymd H:i:s'));
+        $this->virtualTypeLogger->err("This is a message written by " . get_class($this->virtualTypeLogger) . " at " . date('Ymd H:i:s'));
+        $this->virtualTypeLogger->critical("This is a message written by " . get_class($this->virtualTypeLogger) . " at " . date('Ymd H:i:s'));
+        $this->virtualTypeLogger->alert("This is a message written by " . get_class($this->virtualTypeLogger) . " at " . date('Ymd H:i:s'));
+        $this->virtualTypeLogger->emergency("This is a message written by " . get_class($this->virtualTypeLogger) . " at " . date('Ymd H:i:s'));
+
+        $output->writeln("All done!");
     }
-} 
+}
