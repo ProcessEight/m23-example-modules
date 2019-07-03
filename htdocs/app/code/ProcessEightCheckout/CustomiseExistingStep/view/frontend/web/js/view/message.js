6abca9d032f14909398c9f0740746e0e2e1544f3
ProcessEight <s.frost@frostnet.co.uk>
2019-07-03 15:10:42 +0100

    ProcessEightCheckout_CustomiseExistingStep: Demonstrates how to modify part of an existing checkout step

---
+++ htdocs/app/code/ProcessEightCheckout/CustomiseExistingStep/view/frontend/web/js/view/message.js
@@ -0,0 +1,29 @@
+/**
+ * ProcessEightCheckout
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact ProcessEightCheckout for more information.
+ *
+ * @package     m23-example-modules
+ * @copyright   Copyright (c) 2019 ProcessEightCheckout
+ * @author      ProcessEightCheckout
+ *
+ */
+
+define([
+    'uiComponent',
+    'Magento_Shipping/js/model/config'
+
+], function (Component, config) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'ProcessEightCheckout_CustomiseExistingStep/message'
+        },
+        config: config()
+    });
+});
